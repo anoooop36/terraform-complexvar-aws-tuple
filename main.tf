40a082143f7d43fa796548b7f3faa13b6022de87
Anoop Chaudhary <anoopchaudhary36@gmail.com>
2022-06-17 23:26:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ provider "aws" {
  access_key = "${   var.access_key }"
  secret_key = var.secret_key
  region     = "${  var.region}"
- version    = ">= 4.19.0"
+ version    = ">= 3.0"
 }
 
  resource "aws_ebs_volume" "awsEbsExample" {
