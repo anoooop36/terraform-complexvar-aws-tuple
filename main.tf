df061149f75fb0070b8ce1a28a01d48a98d8af6d
Anoop Chaudhary <anoopchaudhary36@gmail.com>
2022-06-17 22:43:19 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,23 @@
+variable "access_key" {}
+variable "secret_key" {}
+
+variable "region" {
+ default = "us-east-1"
+}
+
+
+provider "aws" {
+ access_key = "${   var.access_key }"
+ secret_key = var.secret_key
+ region     = "${  var.region}"
+ version    = ">= 4.19.0"
+}
+
+ resource "aws_ebs_volume" "awsEbsExample" {
+  availability_zone = "us-east-1a"
+  size              = var.sampleTuple[0]
+
+  tags = {
+    Name = var.sampleTuple[1]
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "sampleTuple" {
+  type = tuple([number,string,string])
+  default = [40, "Tuple Volume Tag", "Third Volume Tag"]
+}
