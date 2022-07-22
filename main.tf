60d80ed4c1f42065268eb2b4b7563022c1d55b55
Anoop Chaudhary <anoopchaudhary36@gmail.com>
2022-07-22 21:42:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,23 +1,13 @@
-variable "access_key" {}
-variable "secret_key" {}
-
-variable "region" {
- default = "us-east-1"
-}
-
-
 provider "aws" {
- access_key = "${   var.access_key }"
+ access_key = var.access_key
  secret_key = var.secret_key
- region     = "${  var.region}"
- version    = ">= 3.0"
+ region     = var.region
 }
 
- resource "aws_ebs_volume" "awsEbsExample" {
+resource "aws_ebs_volume" "awsEbsExampleWithTuple" {
   availability_zone = "us-east-1a"
   size              = var.sampleTuple[0]
-
-  tags = {
-    Name = var.sampleTuple[1]
-  }
+  tags              = {
+                        Name = var.sampleTuple[1]
+                      }
 }
